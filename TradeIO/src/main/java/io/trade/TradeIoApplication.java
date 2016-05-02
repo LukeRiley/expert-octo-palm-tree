e2e0b5c6e314b67cb83ea6c64ee0770faf50df23
Luke Riley <luke_riley@outlook.com>
2016-05-03 09:40:20 +1200

    Initial Commit
    Should contain start up project

---
+++ TradeIO/src/main/java/io/trade/TradeIoApplication.java
@@ -0,0 +1,12 @@
+package io.trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeIoApplication.class, args);
+	}
+}
