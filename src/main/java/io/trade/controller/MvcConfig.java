f18d6c2d31c3ed6465932af7c1e2c1ca8780cb17
Luke Riley <luke_riley@outlook.com>
2016-05-03 10:17:10 +1200

    Improved structure with packages

--- src/main/java/io/trade/controller/MvcConfig.java
+++ src/main/java/io/trade/controller/MvcConfig.java
@@ -1,4 +1,4 @@
-package io.trade;
+package io.trade.controller;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
--- src/main/java/io/trade/security/WebSecurityConfig.java
+++ src/main/java/io/trade/security/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package io.trade;
+package io.trade.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
