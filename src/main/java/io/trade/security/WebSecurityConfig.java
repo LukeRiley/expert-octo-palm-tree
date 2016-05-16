aa5dd5b0384702fb0754314d693f0c061157719a
Nathan Cooper <nathan.cooper.nzl@gmail.com>
2016-05-16 16:05:03 +1200

    Register now adds users to database
    Still needs to create related roles in order to enable login

--- src/main/java/io/trade/security/WebSecurityConfig.java
+++ src/main/java/io/trade/security/WebSecurityConfig.java
@@ -31,7 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	  .and()
 	    .logout().logoutSuccessUrl("/login?logout") 
 	  .and()
-	    .csrf();
+	    .csrf().disable();
 	 }
 
     @Autowired
