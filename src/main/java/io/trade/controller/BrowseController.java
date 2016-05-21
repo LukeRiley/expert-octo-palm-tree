0add43d13c9a4c071773e957c1c091612324a0f7
Peter Scriven <justabitdope+git@gmail.com>
2016-05-21 16:27:25 +1200

    Tiny browse edits

--- src/main/java/io/trade/controller/BrowseController.java
+++ src/main/java/io/trade/controller/BrowseController.java
@@ -1,6 +1,5 @@
 package io.trade.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,6 +8,7 @@ public class BrowseController {
 	
 	@RequestMapping(value = "/browse")
 	public String browse() {
+		
 		return "browse";
 	}
 	
